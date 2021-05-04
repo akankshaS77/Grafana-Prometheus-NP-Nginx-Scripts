01e63c8d75e7752e407b15cc02de5cf56323f69c
Akanksha Singh <akanksha.6698@gmail.com>
2021-05-04 14:00:18 +0530

    grafana

--- grafana.sh
+++ grafana.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
 sudo add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"
