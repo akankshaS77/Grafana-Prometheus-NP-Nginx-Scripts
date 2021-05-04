70ca0cf284818695c57da0d6ea7b0e4ac1bc22d6
Akanksha Singh <akanksha.6698@gmail.com>
2021-05-04 14:26:20 +0530

    Grafana Shell Script

--- grafana.sh
+++ grafana.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
 sudo add-apt-repository "deb https://packages.grafana.com/oss/deb stable main"
@@ -7,6 +7,3 @@ sudo apt -y install grafana
 sudo systemctl start grafana-server
 sudo systemctl enable grafana-server
 sudo systemctl status grafana-server
-
-
-
